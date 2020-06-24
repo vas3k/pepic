1d04f5fb5e9f23b123b6401b10a5bf07e4995662
vas3k <me@vas3k.ru>
2020-06-24 23:04:34 +0200

    Fix splitFileNameNGrams

--- processing/utils.go
+++ processing/utils.go
@@ -36,7 +36,7 @@ func splitFileNameNGrams(filename string, n int, stop int) string {
 	resultBuilder.Grow(len(base) + 5)
 	for i, r := range base {
 		resultBuilder.WriteRune(r)
-		if (i + 1) % n == 0 && i != len(base) {
+		if (i + 1) % n == 0 && i < len(base) - 1 {
 			resultBuilder.WriteRune('/')
 		}
 		if i >= stop {
